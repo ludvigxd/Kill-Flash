51aef3d35215aa07009f9e955eeec1f4506b04ff
ludvigxd <96202918+ludvigxd@users.noreply.github.com>
2021-12-15 20:00:28 +0100

    Kill flash
    dette er kill flash der gør så det ser fedre ud når i skyder norgen så kommer der effekter
    
    download vis du vil
    
    her er min fivem udvikling discord:https://discord.gg/VYyJCu49DF

---
+++ Gangversion/Server.lua
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="UTF-8"?>
+                                                                                                                                                                                                                                   <!--           
+  by Jordberg#3323                                                         
+                                                                                                                                                                                                                                            -->
+<timecycle_modifier_data version="1.000000">
+	<modifier name="default" numMods="0" userFlags="0" />
+	<modifier name="hud_def_blur_switch" numMods="0" userFlags="0"/>
+	<modifier name="hud_hd_blur" numMods="0" userFlags="0"/>
+	<modifier name="player_transition" numMods="0" userFlags="0" />
+	<modifier name="player_transition_no_scanlines" numMods="0" userFlags="0" />
+	<modifier name="player_transition_scanlines" numMods="0" userFlags="0" />
+	<modifier name="phone_cam13" numMods="0" userFlags="0"/>
+	<modifier name="hud_def_lensdistortion" numMods="1" userFlags="0">
+		<lens_dist_coeff>-0.200 0.000</lens_dist_coeff>
+	</modifier>
+	<modifier name="hud_def_lensdistortion_subtle" numMods="1" userFlags="0">
+		<lens_dist_coeff>-0.100 0.000</lens_dist_coeff>
+	</modifier>
+	<modifier name="hud_def_blur" numMods="2" userFlags="0">
+		<!-->PAUZA-->
+		<screen_blur_intensity>0.000 0.000</screen_blur_intensity>
+		<postfx_desaturation>1.000 0.000</postfx_desaturation>
+	</modifier>
+	<modifier name="hud_def_colorgrade" numMods="23" userFlags="0">
+		<postfx_shift_col_r>0.227 1.000</postfx_shift_col_r>
+		<postfx_shift_col_g>0.569 1.000</postfx_shift_col_g>
+		<postfx_shift_col_b>1.000 1.000</postfx_shift_col_b>
+		<postfx_shift_cutoff>0.941 0.000</postfx_shift_cutoff>
+		<postfx_desaturation>0.800 0.000</postfx_desaturation>
+		<postfx_tonemap_filmic_override_dark>1.000 0.000</postfx_tonemap_filmic_override_dark>
+		<postfx_tonemap_filmic_exposure_dark>1.000 0.000</postfx_tonemap_filmic_exposure_dark>
+		<postfx_tonemap_filmic_a>1.000 0.000</postfx_tonemap_filmic_a>
+		<postfx_tonemap_filmic_b>0.235 0.000</postfx_tonemap_filmic_b>
+		<postfx_tonemap_filmic_c>0.383 0.000</postfx_tonemap_filmic_c>
+		<postfx_tonemap_filmic_d>0.395 0.000</postfx_tonemap_filmic_d>
+		<postfx_tonemap_filmic_e>0.010 0.000</postfx_tonemap_filmic_e>
+		<postfx_tonemap_filmic_f>0.300 0.000</postfx_tonemap_filmic_f>
+		<postfx_tonemap_filmic_w>4.000 0.000</postfx_tonemap_filmic_w>
+		<postfx_tonemap_filmic_override_bright>1.000 0.000</postfx_tonemap_filmic_override_bright>
+		<postfx_tonemap_filmic_exposure_bright>-2.000 0.000</postfx_tonemap_filmic_exposure_bright>
+		<postfx_tonemap_filmic_a_bright>0.572 0.000</postfx_tonemap_filmic_a_bright>
+		<postfx_tonemap_filmic_b_bright>0.300 0.000</postfx_tonemap_filmic_b_bright>
+		<postfx_tonemap_filmic_c_bright>0.103 0.000</postfx_tonemap_filmic_c_bright>
+		<postfx_tonemap_filmic_d_bright>0.225 0.000</postfx_tonemap_filmic_d_bright>
+		<postfx_tonemap_filmic_e_bright>0.010 0.000</postfx_tonemap_filmic_e_bright>
+		<postfx_tonemap_filmic_f_bright>0.300 0.000</postfx_tonemap_filmic_f_bright>
+		<postfx_tonemap_filmic_w_bright>4.000 0.000</postfx_tonemap_filmic_w_bright>
+	</modifier>
+	<modifier name="hud_def_flash" numMods="7" userFlags="0">
+		<postfx_desaturation>0.310 0.000</postfx_desaturation>
+		<postfx_tonemap_filmic_override_dark>1.000 0.000</postfx_tonemap_filmic_override_dark>
+		<postfx_tonemap_filmic_exposure_dark>1.000 0.000</postfx_tonemap_filmic_exposure_dark>
+		<postfx_tonemap_filmic_a>0.833 0.000</postfx_tonemap_filmic_a>
+		<postfx_tonemap_filmic_b>0.000 0.000</postfx_tonemap_filmic_b>
+		<postfx_tonemap_filmic_d>0.032 0.000</postfx_tonemap_filmic_d>
+		<postfx_tonemap_filmic_f>0.474 0.000</postfx_tonemap_filmic_f>
+	</modifier>
+	<modifier name="hud_def_desatcrunch" numMods="23" userFlags="0">
+		<postfx_bright_pass_thresh>0.000 0.000</postfx_bright_pass_thresh>
+		<postfx_intensity_bloom>0.000 0.000</postfx_intensity_bloom>
+		<postfx_correct_col_r>1.000 0.000</postfx_correct_col_r>
+		<postfx_correct_col_g>1.000 0.000</postfx_correct_col_g>
+		<postfx_correct_col_b>1.000 0.000</postfx_correct_col_b>
+		<postfx_correct_cutoff>0.000 0.000</postfx_correct_cutoff>
+		<postfx_shift_col_r>0.000 1.000</postfx_shift_col_r>
+		<postfx_shift_col_g>0.000 1.000</postfx_shift_col_g>
+		<postfx_shift_col_b>0.000 1.000</postfx_shift_col_b>
+		<postfx_tonemap_filmic_override_dark>1.000 0.000</postfx_tonemap_filmic_override_dark>
+		<postfx_tonemap_filmic_exposure_dark>1.000 0.000</postfx_tonemap_filmic_exposure_dark>
+		<postfx_tonemap_filmic_a>16.000 0.000</postfx_tonemap_filmic_a>
+		<postfx_tonemap_filmic_b>0.040 0.000</postfx_tonemap_filmic_b>
+		<postfx_tonemap_filmic_c>0.015 0.000</postfx_tonemap_filmic_c>
+		<postfx_tonemap_filmic_d>0.116 0.000</postfx_tonemap_filmic_d>
+		<postfx_tonemap_filmic_w>0.275 0.000</postfx_tonemap_filmic_w>
+		<postfx_tonemap_filmic_override_bright>1.000 0.000</postfx_tonemap_filmic_override_bright>
+		<postfx_tonemap_filmic_exposure_bright>-2.000 0.000</postfx_tonemap_filmic_exposure_bright>
+		<postfx_tonemap_filmic_a_bright>2.688 0.000</postfx_tonemap_filmic_a_bright>
+		<postfx_tonemap_filmic_b_bright>0.747 0.000</postfx_tonemap_filmic_b_bright>
+		<postfx_tonemap_filmic_c_bright>0.021 0.000</postfx_tonemap_filmic_c_bright>
+		<postfx_tonemap_filmic_d_bright>1.000 0.000</postfx_tonemap_filmic_d_bright>
+		<postfx_tonemap_filmic_f_bright>0.056 0.000</postfx_tonemap_filmic_f_bright>
+	</modifier>
+	<modifier name="hud_def_desat_switch" numMods="18" userFlags="0">
+		<postfx_tonemap_filmic_override_dark>1.000 0.000</postfx_tonemap_filmic_override_dark>
+		<postfx_tonemap_filmic_exposure_dark>1.000 0.000</postfx_tonemap_filmic_exposure_dark>
+		<postfx_tonemap_filmic_a>1.000 0.000</postfx_tonemap_filmic_a>
+		<postfx_tonemap_filmic_b>0.235 0.000</postfx_tonemap_filmic_b>
+		<postfx_tonemap_filmic_c>0.383 0.000</postfx_tonemap_filmic_c>
+		<postfx_tonemap_filmic_d>0.395 0.000</postfx_tonemap_filmic_d>
+		<postfx_tonemap_filmic_e>0.010 0.000</postfx_tonemap_filmic_e>
+		<postfx_tonemap_filmic_f>0.300 0.000</postfx_tonemap_filmic_f>
+		<postfx_tonemap_filmic_w>4.000 0.000</postfx_tonemap_filmic_w>
+		<postfx_tonemap_filmic_override_bright>1.000 0.000</postfx_tonemap_filmic_override_bright>
+		<postfx_tonemap_filmic_exposure_bright>-2.000 0.000</postfx_tonemap_filmic_exposure_bright>
+		<postfx_tonemap_filmic_a_bright>0.572 0.000</postfx_tonemap_filmic_a_bright>
+		<postfx_tonemap_filmic_b_bright>0.300 0.000</postfx_tonemap_filmic_b_bright>
+		<postfx_tonemap_filmic_c_bright>0.103 0.000</postfx_tonemap_filmic_c_bright>
+		<postfx_tonemap_filmic_d_bright>0.225 0.000</postfx_tonemap_filmic_d_bright>
+		<postfx_tonemap_filmic_e_bright>0.010 0.000</postfx_tonemap_filmic_e_bright>
+		<postfx_tonemap_filmic_f_bright>0.300 0.000</postfx_tonemap_filmic_f_bright>
